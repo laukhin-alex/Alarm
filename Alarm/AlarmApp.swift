270682bf51f13b4cff9f40856bf4ff8dfbf53b6c
laukhin-alex <alex.laukhin@yandex.ru>
2023-03-05 15:25:04 +0400

    Initial Commit

---
+++ Alarm/AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmApp.swift
+//  Alarm
+//
+//  Created by Александр on 05.03.23.
+//
+
+import SwiftUI
+
+@main
+struct AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
